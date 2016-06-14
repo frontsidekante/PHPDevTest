336c9ca67d2c7d137f10ec0261a8251ee41b4c13
Vicky <v.koehring@gmail.com>
2016-06-14 18:38:05 +0200

    Initial commit

---
+++ src/urlrequest.php
@@ -0,0 +1,47 @@
+/*
+* author: Vicky
+* http://codular.com/curl-with-php
+*/
+
+<html>
+    <body>
+        <form action="urlrequest.php" method="post">
+            City: <input type="text" name="name"><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+
+        <?php
+        if (isset($_POST['submit'])) {
+            // returns a cURL resource, takes URL as parameter
+            $curl = curl_init();
+            // SETTING URL
+            //curl_setopt($curl, CURLOPT_URL, 'http://api.goeuro.com/api/v2/position/suggest/en/berlin');
+
+            // SETTING MORE VALUES
+            curl_setopt_array($curl, array(
+                // RETURNS RESPONSE AS STRING INSTEAD OF OUTPUTTING IT ON THE SCREEN
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => 'http://api.goeuro.com/api/v2/position/suggest/en/' . $_POST["name"]
+            ));
+
+            //echo $curl;
+
+            // EXECUTES CURL-REQUEST
+            $result = curl_exec($curl);
+
+
+            // CREATES FILE
+            $city = fopen(__DIR__ . "/.." . "/output/" . $_POST["name"] . ".txt", "w") or die("Unable to open file");
+
+            // fwrite(RESOURCE, STRING)
+            fwrite($city, $result);
+            fclose($city);
+            echo $result;
+            //echo gettype($result);
+
+            // CLOSES CURL REQUEST
+            curl_close($curl);
+        }
+        ?>
+    </body>
+</html>
